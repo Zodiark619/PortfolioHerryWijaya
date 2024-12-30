7d6abb7c7b88c45c1cf4a13f44d1f0a85a0c3711
asd asd <seonghyejin33@gmail.com>
2024-12-30 23:51:05 +0700

    Add project files.

---
+++ PortfolioHerryWijaya/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortfolioHerryWijaya.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
