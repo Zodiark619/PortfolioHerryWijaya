2bad043f2c6b33c7a10f35692f69ca3f856dc544
asd asd <seonghyejin33@gmail.com>
2025-02-16 22:12:20 +0700

    windows form app - home page

--- PortfolioHerryWijaya2/Form1.Designer.cs
+++ PortfolioHerryWijaya2/Form1.Designer.cs
@@ -28,43 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            listView1 = new ListView();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label6 = new Label();
+            pictureBox1 = new PictureBox();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            menuStrip1 = new MenuStrip();
+            project1ToolStripMenuItem = new ToolStripMenuItem();
+            homeToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            button1.Location = new Point(212, 273);
-            button1.Name = "button1";
-            button1.Size = new Size(359, 107);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.Location = new Point(319, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Herry Wijaya";
             // 
-            // listView1
+            // label2
             // 
-            listView1.Location = new Point(123, 90);
-            listView1.Name = "listView1";
-            listView1.Size = new Size(121, 97);
-            listView1.TabIndex = 1;
-            listView1.UseCompatibleStateImageBehavior = false;
+            label2.AutoSize = true;
+            label2.Location = new Point(319, 123);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 15);
+            label2.TabIndex = 1;
+            label2.Text = "C# / .NET Developer";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(319, 181);
+            label3.Name = "label3";
+            label3.Size = new Size(133, 15);
+            label3.TabIndex = 2;
+            label3.Text = "065116076@unpak.ac.id";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(319, 355);
+            label6.Name = "label6";
+            label6.Size = new Size(85, 15);
+            label6.TabIndex = 5;
+            label6.Text = "0895632067037";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(25, 65);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(273, 305);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(319, 301);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(296, 15);
+            linkLabel1.TabIndex = 7;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "https://www.linkedin.com/in/herry-wijaya-78b7a5216/";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(319, 239);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(202, 15);
+            linkLabel2.TabIndex = 8;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "https://www.github.com/Zodiark619";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { project1ToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 9;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // project1ToolStripMenuItem
+            // 
+            project1ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { homeToolStripMenuItem });
+            project1ToolStripMenuItem.Name = "project1ToolStripMenuItem";
+            project1ToolStripMenuItem.Size = new Size(65, 20);
+            project1ToolStripMenuItem.Text = "Project 1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            homeToolStripMenuItem.Size = new Size(180, 22);
+            homeToolStripMenuItem.Text = "Home";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(listView1);
-            Controls.Add(button1);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private ListView listView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label6;
+        private PictureBox pictureBox1;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem project1ToolStripMenuItem;
+        private ToolStripMenuItem homeToolStripMenuItem;
     }
 }
--- PortfolioHerryWijaya2/Form1.cs
+++ PortfolioHerryWijaya2/Form1.cs
@@ -1,4 +1,5 @@
 using PortfolioHerryWijaya2.Model;
+using System.Diagnostics;
 
 namespace PortfolioHerryWijaya2
 {
@@ -11,8 +12,20 @@ namespace PortfolioHerryWijaya2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            var meong = new Gacha();
-           var gogo= meong.SeedData();
+            // var meong = new Gacha();
+            // var gogo = meong.SeedData();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            
+            Process.Start(new ProcessStartInfo("https://www.github.com/Zodiark619") { UseShellExecute = true });
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://www.linkedin.com/in/herry-wijaya-78b7a5216/") { UseShellExecute = true });
+
         }
     }
 }
